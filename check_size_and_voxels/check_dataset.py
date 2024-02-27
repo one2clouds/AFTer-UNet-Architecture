3299b9aaafd3d2af3ff10e5e8c4fe952c434019d
Shirshak <shirshak.acharya@gmail.com>
2024-02-27 15:47:05 +0545

    necessary changes

--- check_size_and_voxels/check_dataset.py
+++ check_size_and_voxels/check_dataset.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from tqdm import tqdm 
 import nibabel as nib 
+import glob
 
 def print_max_and_min_values(df):
     values = df['image'].apply(lambda x: x[0])
@@ -10,19 +11,29 @@ def print_max_and_min_values(df):
     print(f'Y_max value is :{values.apply(lambda x: x[1]).max()}')
 
 
-def check_dataset(image, segmentation):
+
+
+
+image_location = '/mnt/Enterprise2/shirshak/SegTHOR/train/P*/P*.nii.gz'
+mask_location = '/mnt/Enterprise2/shirshak/SegTHOR/train/P*/G*.nii.gz'
+
+
+def check_dataset(image_location, mask_location):
+    image_list = sorted(glob.glob(image_location))
+    mask_list = sorted(glob.glob(mask_location))
+
     columns = ['image', 'segmentation']
     df = pd.DataFrame(columns=columns)
 
-    for index in tqdm(range(len(image[:25]))):
-        image_size=nib.load(image[index]).get_fdata()
-        image_voxel_sizes = nib.load(image[index]).header.get_zooms()
+    for index in tqdm(range(len(image_list[:25]))):
+        image_size=nib.load(image_list[index]).get_fdata()
+        image_voxel_sizes = nib.load(image_list[index]).header.get_zooms()
 
-        seg_size=nib.load(segmentation[index]).get_fdata()
-        seg_voxel_sizes = nib.load(segmentation[index]).header.get_zooms()
+        seg_size=nib.load(mask_list[index]).get_fdata()
+        seg_voxel_sizes = nib.load(mask_list[index]).header.get_zooms()
 
         df.loc[index+1, 'image'] = image_size[index].shape, image_voxel_sizes
         df.loc[index+1, 'segmentation'] = seg_size[index].shape, seg_voxel_sizes
 
-    print_max_and_min_values(df)
-    df.to_csv('utils/sizes_and_voxels.csv')
+    # print_max_and_min_values(df)
+    df.to_csv('check_size_and_voxels/sizes_and_voxels.csv')
--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 from Data.prepare_dataloader import get_from_loader
 import torch
-from dice import DiceLoss
 import torch.optim as optim
 from axial_fusion_transformer import axial_fusion_transformer
 import os
@@ -10,12 +9,17 @@ from operator import add
 import yaml 
 import json
 import matplotlib.pyplot as plt
+from check_size_and_voxels.check_dataset import check_dataset
+import wandb
+from monai.losses import DiceLoss
+from monai.metrics import LossMetric
+
 
 with open('./config/train_config.yaml', 'r') as config_file:
     config_params = yaml.safe_load(config_file)
     model_config = json.dumps(config_params)
 
-def training_phase(train_dataloader, test_dataloader, num_classes):
+def training_phase(train_dataloader, test_dataloader, num_classes, wandb):
     overall_train_loss_per_epoch = []
     overall_test_loss_per_epoch = []
     overall_train_jaccard_per_epoch = []
@@ -23,7 +27,7 @@ def training_phase(train_dataloader, test_dataloader, num_classes):
     overall_train_acc_per_epoch = []
     overall_test_acc_per_epoch = []
 
-    num_epochs = config_params["training_params"]["num_epochs"]
+    num_epochs = wandb.config['epochs']
 
     Na = config_params["training_params"]["Na"]
     Nf = config_params["training_params"]["Nf"]
@@ -32,8 +36,10 @@ def training_phase(train_dataloader, test_dataloader, num_classes):
 
     if config_params["training_params"]["model_name"] == "axial_fusion_transformer":
         model = axial_fusion_transformer(Na,Nf, num_classes).to(device)
-    loss_function = DiceLoss()
-    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+    loss_function = DiceLoss(include_background=True)
+    dice_metric = LossMetric(loss_function)
+    
+    optimizer = optim.Adam(model.parameters(), lr=wandb.config['lr'])
 
 
     checkpoint_path = 'model.pth'
@@ -61,13 +67,17 @@ def training_phase(train_dataloader, test_dataloader, num_classes):
             mask = img_and_mask['_3d_mask']['data'].float().to(device)
             optimizer.zero_grad()
             output = model(image)
-    #         print(output.shape) # torch.Size([4, 128, 128, 128])
-    #         print(mask.squeeze(dim=0).shape) # torch.Size([1, 4, 128, 128, 128])
+
+            # print(output.shape) # torch.Size([5, 128, 128, 128])
+            # print(mask.shape) # torch.Size([1, 5, 128, 128, 128])
+
             train_loss = loss_function(output, mask.squeeze(dim=0))
             train_loss.backward()
 
             score = calculate_metrics(output, mask)
             metrics_score = list(map(add,metrics_score, score))
+
+            dice_score_train = dice_metric(output, mask.squeeze(dim=0))
             
             optimizer.step()
             epoch_train_loss += train_loss.item()
@@ -93,7 +103,8 @@ def training_phase(train_dataloader, test_dataloader, num_classes):
                 
                 score = calculate_metrics(output, mask)
                 metrics_score = list((map(add, metrics_score, score)))
-                optimizer.step()
+
+                dice_score_test = dice_metric(output, mask.squeeze(dim=0))
                 
                 epoch_test_loss += test_loss.item()
                 epoch_test_loss = epoch_train_loss/len(test_dataloader)
@@ -109,7 +120,14 @@ def training_phase(train_dataloader, test_dataloader, num_classes):
             overall_test_loss_per_epoch.append(test_loss.item()) 
             overall_test_jaccard_per_epoch.append(epoch_test_jaccard)
             overall_test_acc_per_epoch.append(epoch_test_acc)
-            
+
+        wandb.log({"train_loss":train_loss.item(),
+                   "test_loss":test_loss.item(),
+                   "train_jaccard":epoch_train_jaccard,
+                   "test_jaccard":epoch_test_jaccard,
+                   "train_dice":dice_score_train,
+                   "test_dice":dice_score_test
+                   })    
         
         print(f'Epoch [{epoch + 1}/{num_epochs}], '
             f'Train Loss: {train_loss.item():.4f}, '
@@ -129,49 +147,62 @@ def training_phase(train_dataloader, test_dataloader, num_classes):
 if __name__ =='__main__':
     image_location = '/mnt/Enterprise2/shirshak/SegTHOR/train/P*/P*.nii.gz'
     mask_location = '/mnt/Enterprise2/shirshak/SegTHOR/train/P*/G*.nii.gz'
+
+    wandb.login(key=config_params["training_params"]["wandb_key"])
+
+    wandb.init(
+        project="SegTHOR Axial Implementation",
+        config={
+            "epochs": config_params["training_params"]["num_epochs"],
+            "batch_size":config_params["training_params"]["batch_size"],
+            "lr": config_params["training_params"]["lr"]
+        }
+    )
+# check for voxel shape and load in csv format
+    # check_dataset(image_location, mask_location)
     
-    train_dataloader, test_dataloader = get_from_loader(image_location, mask_location, config_params["training_params"]["num_classes"])
+    train_dataloader, test_dataloader = get_from_loader(image_location, mask_location, config_params["training_params"]["num_classes"], wandb.config['batch_size'])
                                                         
-    # model, num_epochs,optimizer, loss, overall_train_loss_per_epoch, overall_train_jaccard_per_epoch, overall_train_acc_per_epoch, overall_test_loss_per_epoch, overall_test_jaccard_per_epoch, overall_test_acc_per_epoch = training_phase(train_dataloader,test_dataloader, config_params["training_params"]["num_classes"])
+    model, num_epochs,optimizer, loss, overall_train_loss_per_epoch, overall_train_jaccard_per_epoch, overall_train_acc_per_epoch, overall_test_loss_per_epoch, overall_test_jaccard_per_epoch, overall_test_acc_per_epoch = training_phase(train_dataloader,test_dataloader, config_params["training_params"]["num_classes"], wandb)
 
-    Na = config_params["training_params"]["Na"]
-    Nf = config_params["training_params"]["Nf"]
+    # Na = config_params["training_params"]["Na"]
+    # Nf = config_params["training_params"]["Nf"]
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model = axial_fusion_transformer(Na,Nf, config_params["training_params"]["num_classes"]).to(device)
+    # model = axial_fusion_transformer(Na,Nf, config_params["training_params"]["num_classes"]).to(device)
     
-    model.load_state_dict(torch.load('model.pth')['model_state_dict'])
-    model.eval()
+    # model.load_state_dict(torch.load('model.pth')['model_state_dict'])
+    # model.eval()
 
-    i=0
-    for image_and_mask in list(test_dataloader)[:10]:
-        fig, ax = plt.subplots(1,3)
-        image = image_and_mask['_3d_image']['data'].float().to(device)
-        mask = image_and_mask['_3d_mask']['data'].float().to(device)
-        output = model(image)
+    # i=0
+    # for image_and_mask in list(test_dataloader)[:10]:
+    #     fig, ax = plt.subplots(1,3)
+    #     image = image_and_mask['_3d_image']['data'].float().to(device)
+    #     mask = image_and_mask['_3d_mask']['data'].float().to(device)
+    #     output = model(image)
 
-        image = image.squeeze()
-        #         print(mask.shape)
-        mask = mask.squeeze(dim=0).argmax(0)
-        output = output.squeeze(dim=0).argmax(0)
-        #         print(image.shape) # torch.Size([1, 1, 128, 128, 128])
-        #         print(image.max()) # tensor(1., device='cuda:0')
-        #         print(image.min()) # tensor(0., device='cuda:0')
-        #         print(mask.max()) # tensor(1., device='cuda:0')
-        #         print(mask.min()) # tensor(0., device='cuda:0')
-        #         print(output.max()) #tensor(0.9952, device='cuda:0')
-        #         print(output.min()) # tensor(6.8929e-08, device='cuda:0')
-        #         print(output.shape) # torch.Size([4, 128, 128, 128])
+    #     image = image.squeeze()
+    #     #         print(mask.shape)
+    #     mask = mask.squeeze(dim=0).argmax(0)
+    #     output = output.squeeze(dim=0).argmax(0)
+    #     #         print(image.shape) # torch.Size([1, 1, 128, 128, 128])
+    #     #         print(image.max()) # tensor(1., device='cuda:0')
+    #     #         print(image.min()) # tensor(0., device='cuda:0')
+    #     #         print(mask.max()) # tensor(1., device='cuda:0')
+    #     #         print(mask.min()) # tensor(0., device='cuda:0')
+    #     #         print(output.max()) #tensor(0.9952, device='cuda:0')
+    #     #         print(output.min()) # tensor(6.8929e-08, device='cuda:0')
+    #     #         print(output.shape) # torch.Size([4, 128, 128, 128])
 
-        n_slice = 55
-        ax[0].imshow(image[:,:,n_slice].cpu())
-        ax[0].set_title('Image of SegTHOR CT Scan')
-        ax[1].imshow(mask[:,:,n_slice].cpu())
-        ax[1].set_title('Original Mask')
-        ax[2].imshow(output[:,:,n_slice].cpu() > 0.5)
-        ax[2].set_title('Predicted Mask')
+    #     n_slice = 55
+    #     ax[0].imshow(image[:,:,n_slice].cpu())
+    #     ax[0].set_title('Image of SegTHOR CT Scan')
+    #     ax[1].imshow(mask[:,:,n_slice].cpu())
+    #     ax[1].set_title('Original Mask')
+    #     ax[2].imshow(output[:,:,n_slice].cpu() > 0.5)
+    #     ax[2].set_title('Predicted Mask')
 
-        fig.savefig(f'images/full_figure{i}.png')
-        i += 1
+    #     fig.savefig(f'images/full_figure{i}.png')
+    #     i += 1
 
